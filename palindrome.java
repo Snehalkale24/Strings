3ff023cd4f59c313f18d6a532fef8f384a70abd3
Snehalkale24 <snehalbkale2004@gmail.com>
2026-08-11 21:42:52 +0530

    add

--- palindrome.java
+++ palindrome.java
@@ -1,21 +1,17 @@
-public class palindrome
-{
-	public static void main(String args[])
-	{
-		String str="madam";
-		String rev=" ";
+public class palindrome {
+    public static void main(String[] args) {
 
-		for(int i=str.length()-1;i>=0;i--)
-		{
-			rev=rev+str.charAt(i);
-		}
+        String str = "madam";
+        String rev = "";
 
-		if(str.equals(rev))
-		{
-			System.out.println("palindrome");
-		}
-		else{
-			System.out.println("not palindrome:");
-		}
-	}
+        for(int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if(str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
 }
