12dae7dbfad6d8a9fd9027a30051b056cc6de429
Snehalkale24 <snehalbkale2004@gmail.com>
2026-08-13 13:47:22 +0530

    add

--- firstnonrepeating.java
+++ firstnonrepeating.java
@@ -1,8 +1,41 @@
-import java.util.*;
+// import java.util.*;
+// public class firstnonrepeating
+// {
+// 	public static void nonrepeat(String str)
+// 	{
+// 		for(int i=0;i<str.length();i++)
+// 		{
+// 			int count=0;
+// 			for(int j=0;j<str.length();j++)
+// 			{
+// 				if(str.charAt(i)==str.charAt(j))
+// 				{
+// 					count++;
+// 				}
+// 			}
+// 			if(count==1)
+// 			{
+// 				System.out.println("first non repating:"+str.charAt(i));
+// 				return;
+// 			}
+// 		}
+// 		System.out.println("no non repating");
+// 	}
+// 	public static void main(String args[])
+// 	{
+// 		Scanner s=new Scanner(System.in);
+// 		System.out.println("enter string:");
+// 		String str=s.nextLine();
+
+// 		nonrepeat(str);
+// 	}
+// }
+
 public class firstnonrepeating
 {
-	public static void nonrepeat(String str)
+	public static void main(String args[])
 	{
+		String str="swiss";
 		for(int i=0;i<str.length();i++)
 		{
 			int count=0;
@@ -15,18 +48,9 @@ public class firstnonrepeating
 			}
 			if(count==1)
 			{
-				System.out.println("first non repating:"+str.charAt(i));
-				return;
+				System.out.println(str.charAt(i));
+				break;
 			}
 		}
-		System.out.println("no non repating");
-	}
-	public static void main(String args[])
-	{
-		Scanner s=new Scanner(System.in);
-		System.out.println("enter string:");
-		String str=s.nextLine();
-
-		nonrepeat(str);
 	}
 }
