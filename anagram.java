12a42f8bd551449d2249bd0ec5b75eb3fd0327cc
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-08 22:11:11 +0530

    add

--- anagram.java
+++ anagram.java
@@ -25,20 +25,20 @@ import java.util.*;
 public class anagram{
 	public static void main(String args[])
 	{
-	  String s1="snehal";
-	  String s2="snehal";
+		String str1="snehal";
+		String str2="kale";
 
-	  char ch[]=s1.toCharArray();
-	  char ch2[]=s2.toCharArray();
+		char ch1[]=str1.toCharArray();
+		char ch2[]=str2.toCharArray();
 
-	  Arrays.sort(ch);
-	  Arrays.sort(ch1);
-	  if(Arrays.equals(ch,ch1))
-	  {
-	  	System.out.println("anagram");
-	  }
-	  else{
-	  	System.out.println("not anagram");
-	  }
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+
+		if(Arrays.equals(ch1,ch2)){
+			System.out.println("anagram");
+		}
+		else{
+			System.out.println("not anagram");
+		}
 	}
 }
