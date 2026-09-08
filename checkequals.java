1bd0c35ebb8b4029cd771ef1fe64ed4bfa901727
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-08 22:20:46 +0530

    add

--- checkequals.java
+++ checkequals.java
@@ -23,13 +23,12 @@
 public class checkequals{
 	public static boolean check(String s1,String s2)
 	{
-		if(s1.length() != s2.length())
-		{
+		if(s1.length()!=s2.length())
 			return false;
-		}
+
 		for(int i=0;i<s1.length();i++)
 		{
-			if(s1.charAt(i)==s2.charAt(i))
+			if(s1.length()==s2.length())
 			{
 				return true;
 			}
@@ -39,8 +38,5 @@ public class checkequals{
 	public static void main(String args[])
 	{
 		System.out.println(check("java","java"));
-	}
-	{
-
 	}
 }
