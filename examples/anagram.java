42890b3c266916fd7879b60831fa2429e2ab6c5a
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-20 00:37:33 +0530

    add

--- examples/anagram.java
+++ examples/anagram.java
@@ -23,3 +23,24 @@
 // }
 
 
+public class anagram{
+	public static void main(String args[])
+	{
+		String str1="snehal";
+		String str2="snehal";
+
+		char arr1[]=str1.toCharArray();
+		char arr2[]=str2.toCharArray();
+
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+
+		if(Arrays.equals(arr1,arr2))
+		{
+			System.out.println("anagram");
+		}
+		else{
+			System.out.println("not angaram");
+		}
+	}
+}
